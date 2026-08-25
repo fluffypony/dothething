74bd3e28b12d2ead2c570f1e97079fc83a0386f1
Riccardo Spagni <ric@spagni.net>
2026-08-25 19:20:35 +0200

    Bump build version

--- dtt.sh
+++ dtt.sh
@@ -3,7 +3,7 @@
 # https://github.com/fluffypony/dothething | https://dotheth.ing
 set -euo pipefail
 
-DTT_VERSION="3.0.6"
+DTT_VERSION="3.0.7"
 _dtt_s="$0"
 [[ "$_dtt_s" != */* ]] && _dtt_s="$(command -v "$_dtt_s" 2>/dev/null || echo "$_dtt_s")"
 DTT_SELF="$(realpath "$_dtt_s" 2>/dev/null || echo "$(cd "$(dirname "$_dtt_s")" && pwd -P)/$(basename "$_dtt_s")")"
